99222cac81e59b06af92781b75da62f24ea89d37
Vijinthika <vvijinthi@gmail.com>
2023-06-26 09:22:07 +0530

    EMpty Files

--- src/main/java/com/ii/testautomation/utils/Utils.java
+++ src/main/java/com/ii/testautomation/utils/Utils.java
@@ -1,12 +1,1 @@
-package com.ii.testautomation.utils;
 
-public class Utils {
-    public static boolean isNotNullAndEmpty(String field) {
-        return field != null && !field.isEmpty();
-    }
-
-    private Utils() {
-    }
-//    private Utils() {
-//    }
-}
