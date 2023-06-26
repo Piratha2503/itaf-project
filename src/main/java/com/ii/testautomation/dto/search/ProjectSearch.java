24b92ec1961b3bf66adac96dfeb4f1b939fdee49
Vijinthika <vvijinthi@gmail.com>
2023-06-26 09:23:29 +0530

    ProjectSearch And Utils Files

---
+++ src/main/java/com/ii/testautomation/dto/search/ProjectSearch.java
@@ -0,0 +1,12 @@
+package com.ii.testautomation.dto.search;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProjectSearch {
+    private String code;
+    private String name;
+
+}
--- src/main/java/com/ii/testautomation/utils/Utils.java
+++ src/main/java/com/ii/testautomation/utils/Utils.java
@@ -1,1 +1,11 @@
+package com.ii.testautomation.utils;
 
+public class Utils {
+    public static boolean isNotNullAndEmpty(String field) {
+        return field != null && !field.isEmpty();
+    }
+
+    private Utils() {
+    }
+
+}
