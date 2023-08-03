673f8876f38b04a0fd1a250e2669bcd5df7fe038
KeerthananB <bkeerthiboss@gmail.com>
2023-08-03 09:46:41 +0530

    Sample Test2

--- src/main/java/com/ii/testautomation/controllers/TestGroupingController.java
+++ src/main/java/com/ii/testautomation/controllers/TestGroupingController.java
@@ -89,8 +89,6 @@ public class TestGroupingController {
                 statusCodeBundle.getCommonSuccessCode(),
                 statusCodeBundle.getSaveTestGroupingSuccessMessage()));
     }
-
-
     @DeleteMapping(value = EndpointURI.TEST_GROUPING_BY_ID)
     public ResponseEntity<Object> deleteTestGroupingById(@PathVariable Long id) {
         if (!testGroupingService.existsByTestGroupingId(id)) {
