6439c0f90db4301657228b49cfe13fa8e813f956
Vijinthika <vvijinthi@gmail.com>
2023-08-03 09:09:57 +0530

    TestGrouping DELETE ALL

--- src/main/java/com/ii/testautomation/repositories/TestGroupingRepository.java
+++ src/main/java/com/ii/testautomation/repositories/TestGroupingRepository.java
@@ -7,22 +7,5 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import java.util.List;
 
 public interface TestGroupingRepository extends JpaRepository<TestGrouping, Long>, QuerydslPredicateExecutor<TestGrouping> {
-    boolean existsByNameIgnoreCaseAndTestCases_SubModule_MainModule_Modules_Project_Id(String name,Long ProjectId);
-
-    boolean existsByNameIgnoreCaseAndTestCases_SubModule_MainModule_Modules_Project_IdAndIdNot(String name,Long projectId, Long id);
-
-    boolean existsByTestCases_IdAndTestCases_SubModule_MainModule_Modules_Project_Id(Long testCaseId,Long projectId);
-
-    List<TestGrouping> findAllTestGroupingByTestCasesId(Long id);
-
-    List<TestGrouping> findAllTestGroupingByTestTypeId(Long id);
-
-    boolean existsByTestCasesId(Long id);
-
-    boolean existsByTestTypeId(Long id);
-
-    List<TestGrouping> findByTestCases_SubModule_MainModule_Modules_Project_Id(Long projectId);
-
-    boolean existsByTestCases_SubModule_MainModule_Modules_ProjectId(Long projectId);
 
 }
--- src/main/java/com/ii/testautomation/service/TestGroupingService.java
+++ src/main/java/com/ii/testautomation/service/TestGroupingService.java
@@ -1,56 +1,5 @@
 package com.ii.testautomation.service;
 
-import com.ii.testautomation.dto.request.TestGroupingRequest;
-import com.ii.testautomation.dto.response.TestGroupingResponse;
-import com.ii.testautomation.dto.search.TestGroupingSearch;
-import com.ii.testautomation.entities.Project;
-import com.ii.testautomation.response.common.PaginatedContentResponse;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.InputStream;
-import java.util.List;
-import java.util.Map;
-
 public interface TestGroupingService {
-    void saveTestGrouping(TestGroupingRequest testGroupingRequest);
 
-    public boolean existsByTestGroupingName(String testGroupingName,Long testCaseId);
-
-    boolean existsByTestGroupingId(Long testGroupingId);
-
-    boolean isUpdateTestGroupingNameExits(String testGroupingName, Long testGroupingId);
-
-     TestGroupingResponse getTestGroupingById(Long testGroupingId);
-
-    List<TestGroupingResponse> getALlTestGroupingByTestCaseId(Long testCaseId);
-
-    List<TestGroupingResponse> getALlTestGroupingByTestTypeId(Long testTypeId);
-
-    List<TestGroupingResponse> multiSearchTestGrouping(Pageable pageable, PaginatedContentResponse.Pagination pagination, TestGroupingSearch testGroupingSearch);
-
-    void deleteTestGroupingById(Long testGroupingId);
-
-    boolean existsByTestCasesId(Long testCaseId);
- //   public boolean allTestCasesInSameProject(Long testCaseId,Long projectId);
-
-    boolean existsByTestTypesId(Long testTypeId);
-
-    Map<Integer, TestGroupingRequest> csvToTestGroupingRequest(InputStream inputStream);
-
-    Map<Integer, TestGroupingRequest> excelToTestGroupingRequest(MultipartFile multipartFile);
-
-    boolean hasExcelFormat(MultipartFile multipartFile);
-
-    boolean hasCsvFormat(MultipartFile multipartFile);
-
-    void addToErrorMessages(Map<String, List<Integer>> errorMessages, String key, int value);
-
-    boolean isExcelHeaderMatch(MultipartFile multipartFile);
-
-    boolean isCSVHeaderMatch(MultipartFile multipartFile);
-
-    List<TestGroupingResponse> getTestGroupingByProjectId(Long id);
-
-    boolean existsByProjectId(Long projectId);
 }
