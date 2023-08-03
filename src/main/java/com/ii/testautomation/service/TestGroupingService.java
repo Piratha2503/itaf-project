474b0a0e81ca1363c8e803f8a8033ff07b35630b
Vijinthika <vvijinthi@gmail.com>
2023-08-03 09:18:19 +0530

    TestGrouping Delete unessary all service and service implmentation

--- src/main/java/com/ii/testautomation/service/TestGroupingService.java
+++ src/main/java/com/ii/testautomation/service/TestGroupingService.java
@@ -13,42 +13,19 @@ import java.util.Map;
 public interface TestGroupingService {
     void saveTestGrouping(TestGroupingRequest testGroupingRequest);
 
-    public boolean existsByTestGroupingName(String testGroupingName,Long testCaseId);
+    public boolean existsByTestGroupingName(String testGroupingName, Long testCaseId);
 
     boolean existsByTestGroupingId(Long testGroupingId);
 
-    boolean isUpdateTestGroupingNameExits(String testGroupingName,Long testCaseId ,Long testGroupingId);
-
-     TestGroupingResponse getTestGroupingById(Long testGroupingId);
-
-    List<TestGroupingResponse> getALlTestGroupingByTestCaseId(Long testCaseId);
-
-    List<TestGroupingResponse> getALlTestGroupingByTestTypeId(Long testTypeId);
-
-    List<TestGroupingResponse> multiSearchTestGrouping(Pageable pageable, PaginatedContentResponse.Pagination pagination, TestGroupingSearch testGroupingSearch);
+    boolean isUpdateTestGroupingNameExits(String testGroupingName, Long testCaseId, Long testGroupingId);
 
     void deleteTestGroupingById(Long testGroupingId);
 
     boolean existsByTestCasesId(Long testCaseId);
-  boolean allTestCasesInSameProject(List<Long> testCaseIds);
+
+    boolean allTestCasesInSameProject(List<Long> testCaseIds);
 
     boolean existsByTestTypesId(Long testTypeId);
 
-    Map<Integer, TestGroupingRequest> csvToTestGroupingRequest(InputStream inputStream);
 
-    Map<Integer, TestGroupingRequest> excelToTestGroupingRequest(MultipartFile multipartFile);
-
-    boolean hasExcelFormat(MultipartFile multipartFile);
-
-    boolean hasCsvFormat(MultipartFile multipartFile);
-
-    void addToErrorMessages(Map<String, List<Integer>> errorMessages, String key, int value);
-
-    boolean isExcelHeaderMatch(MultipartFile multipartFile);
-
-    boolean isCSVHeaderMatch(MultipartFile multipartFile);
-
-    List<TestGroupingResponse> getTestGroupingByProjectId(Long id);
-
-    boolean existsByProjectId(Long projectId);
 }
