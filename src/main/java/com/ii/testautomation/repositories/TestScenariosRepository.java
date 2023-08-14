3fdac3fa627252648309e5eb8a5f91ff77a82684
Piratha2503 <pirathaban1992@gmail.com>
2023-08-14 10:18:48 +0530

    Test Scenario

--- src/main/java/com/ii/testautomation/repositories/TestScenariosRepository.java
+++ src/main/java/com/ii/testautomation/repositories/TestScenariosRepository.java
@@ -3,5 +3,5 @@ package com.ii.testautomation.repositories;
 import com.ii.testautomation.entities.TestScenarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TestScenarioRepository extends JpaRepository<TestScenarios,Long> {
+public interface TestScenariosRepository extends JpaRepository<TestScenarios,Long> {
 }
