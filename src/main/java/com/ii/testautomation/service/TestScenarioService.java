1326045ff862af275b450f5a87d9ce00456f48ac
Piratha2503 <pirathaban1992@gmail.com>
2023-08-14 09:56:58 +0530

    Test Scenario

--- src/main/java/com/ii/testautomation/service/TestScenarioService.java
+++ src/main/java/com/ii/testautomation/service/TestScenarioService.java
@@ -2,5 +2,5 @@ package com.ii.testautomation.service;
 
 public interface TestScenarioService {
     boolean existsByTestScenarioId(Long testScenarioId);
-
+    boolean existsByTestScenarioNameIgnoreCase(String name);
 }
