b82861522a622521efad40fd8bad5b350fd1f33b
Vijinthika <vvijinthi@gmail.com>
2023-08-14 11:30:42 +0530

    Project Upload Check for ubuntu OS

--- src/main/java/com/ii/testautomation/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/ii/testautomation/service/impl/ProjectServiceImpl.java
@@ -33,8 +33,10 @@ import java.util.*;
 @Component
 @PropertySource("classpath:application.properties")
 public class ProjectServiceImpl implements ProjectService {
-    @Value("${jar.import.file.path}")
-    private String fileFolder;
+    @Value("${jar.import.file.windows.path}")
+    private String windowsFileFolder;
+    @Value("${jar.import.file.ubuntu.path}")
+    private String ubuntuFileFolder;
     @Autowired
     private ProjectRepository projectRepository;
 
@@ -67,7 +69,7 @@ public class ProjectServiceImpl implements ProjectService {
     public void saveProject(ProjectRequest projectRequest, MultipartFile jarFile, MultipartFile configFile) {
         Project project = new Project();
         BeanUtils.copyProperties(projectRequest, project);
-        String directoryPath = fileFolder+projectRequest.getName();
+        String directoryPath = ubuntuFileFolder+projectRequest.getName();
         String uploadedJarFilePath = null;
         String uploadedConfigFilePath = null;
         File jarDirectory = new File(directoryPath);
