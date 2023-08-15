4744000393ab37d110c9d6d02b2eddcba62614b1
Piratha2503 <pirathaban1992@gmail.com>
2023-08-15 10:48:46 +0530

    test Scenario save ok with front end

--- src/main/java/com/ii/testautomation/controllers/TestScenariosController.java
+++ src/main/java/com/ii/testautomation/controllers/TestScenariosController.java
@@ -24,32 +24,12 @@ public class TestScenariosController {
     private StatusCodeBundle statusCodeBundle;
 
     @PostMapping(EndpointURI.TEST_SCENARIO)
-    public ResponseEntity<Object> insertTestScenario(@RequestParam(name = "name",required = false) String name,
-                                                     @RequestParam(name = "moduleIds",required = false) List<Long> moduleIds,
-                                                     @RequestParam(name = "mainModuleIds",required = false) List<Long> mainModuleIds,
-                                                     @RequestParam(name = "subModuleIds",required = false) List<Long> subModuleIds,
-                                                     @RequestParam(name = "testCasesId",required = false) List<Long> testCasesId
-                                                     ) throws JsonProcessingException{
-        TestScenariosRequest testScenariosRequest = new TestScenariosRequest();
-        testScenariosRequest.setName(name);
-        if (moduleIds == null) testScenariosRequest.setModuleIds(null);
-        testScenariosRequest.setModuleIds(moduleIds);
-        if (mainModuleIds == null) testScenariosRequest.setMainModuleIds(null);
-        testScenariosRequest.setMainModuleIds(mainModuleIds);
-        if (subModuleIds == null) testScenariosRequest.setSubModuleIds(null);
-        testScenariosRequest.setSubModuleIds(subModuleIds);
-        if (testCasesId == null) testScenariosRequest.setTestCasesId(null);
-        testScenariosRequest.setTestCasesId(testCasesId);
-
-      if (testScenariosService.existsByTestScenarioNameIgnoreCase(testScenariosRequest.getName()))
-        return ResponseEntity.ok(new BaseResponse(RequestStatus.FAILURE.getStatus(), statusCodeBundle.getTestScenariosAlreadyExistCode(),statusCodeBundle.getTestScenariosNameAlreadyExistMessage()));
-
-      else if (!(testScenariosRequest.getTestCasesId()==null) && testScenariosService.existByTestCaseList(testScenariosRequest))
-        return ResponseEntity.ok(new BaseResponse(RequestStatus.FAILURE.getStatus(), statusCodeBundle.getTestScenariosAlreadyExistCode(),statusCodeBundle.getTestCasesListAlreadyExistMessage()));
-        else {
-            testScenariosService.saveTestScenario(testScenariosRequest);
-            return ResponseEntity.ok(new BaseResponse(RequestStatus.SUCCESS.getStatus(), statusCodeBundle.getCommonSuccessCode(), statusCodeBundle.getTestScenariosSaveMessage()));
-        }
+    public ResponseEntity<Object> insertScenario(@RequestBody TestScenariosRequest testScenariosRequest) {
+        if (testScenariosService.existsByTestScenarioNameIgnoreCase(testScenariosRequest.getName()))
+            return ResponseEntity.ok(new BaseResponse(RequestStatus.FAILURE.getStatus(), statusCodeBundle.getTestScenariosAlreadyExistCode(), statusCodeBundle.getTestScenariosNameAlreadyExistMessage()));
+        testScenariosService.saveTestScenario(testScenariosRequest);
+        return ResponseEntity.ok(new BaseResponse(RequestStatus.SUCCESS.getStatus(), statusCodeBundle.getCommonSuccessCode(), statusCodeBundle.getTestScenariosSaveMessage()));
+    }
 
     }
 
