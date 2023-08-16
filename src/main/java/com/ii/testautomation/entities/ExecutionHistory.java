6d8d399b7109838cc2eb5d56ddb4f2b7b18123b7
Vijinthika <vvijinthi@gmail.com>
2023-08-16 17:03:27 +0530

    ProjectEdit changes while doing edit folder name need to rename

---
+++ src/main/java/com/ii/testautomation/entities/ExecutionHistory.java
@@ -0,0 +1,18 @@
+//package com.ii.testautomation.entities;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//
+//@Entity
+//@Getter
+//@Setter
+//public class ExecutionHistory {
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//
+//}
