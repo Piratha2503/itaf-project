934ff0e4aa2cd15b6974fd318864e43115d32aec
Piratha2503 <pirathaban1992@gmail.com>
2023-08-20 12:51:20 +0530

    testGrouping Response implemented

--- src/main/java/com/ii/testautomation/controllers/ExecutionHistoryController.java
+++ src/main/java/com/ii/testautomation/controllers/ExecutionHistoryController.java
@@ -1,6 +1,7 @@
 package com.ii.testautomation.controllers;
 
 import com.ii.testautomation.dto.response.ExecutionHistoryResponse;
+import com.ii.testautomation.entities.TestGrouping;
 import com.ii.testautomation.enums.RequestStatus;
 import com.ii.testautomation.response.common.BaseResponse;
 import com.ii.testautomation.response.common.ContentResponse;
@@ -31,19 +32,20 @@ public class ExecutionHistoryController {
     private StatusCodeBundle statusCodeBundle;
 
     @GetMapping(EndpointURI.EXECUTION_HISTORY_BY_TEST_GROUPING_ID)
-    public ResponseEntity<Object> viewByTestGroupingId(@PathVariable Long id){
+    public ResponseEntity<Object> viewByTestGroupingId(@PathVariable Long id) {
 
         if (!testGroupingService.existsByTestGroupingId(id))
-            return ResponseEntity.ok(new BaseResponse(RequestStatus.FAILURE.getStatus(), statusCodeBundle.getTestGroupingNotExistCode(),statusCodeBundle.getTestGroupingNotExistsMessage()));
-        if(!executionHistoryService.existByTestGropingId(id))
-            return ResponseEntity.ok(new BaseResponse(RequestStatus.FAILURE.getStatus(), statusCodeBundle.getFailureCode(),statusCodeBundle.getTestGroupingNotMappedMessage()));
-        return ResponseEntity.ok(new ContentResponse<>(Constants.EXECUTION_HISTORY, executionHistoryService.viewByTestGroupingId(id), RequestStatus.SUCCESS.getStatus(), statusCodeBundle.getCommonSuccessCode(),statusCodeBundle.getViewExecutionHistoryMessage()));
+            return ResponseEntity.ok(new BaseResponse(RequestStatus.FAILURE.getStatus(), statusCodeBundle.getTestGroupingNotExistCode(), statusCodeBundle.getTestGroupingNotExistsMessage()));
+        if (!executionHistoryService.existByTestGropingId(id))
+            return ResponseEntity.ok(new BaseResponse(RequestStatus.FAILURE.getStatus(), statusCodeBundle.getFailureCode(), statusCodeBundle.getTestGroupingNotMappedMessage()));
+        return ResponseEntity.ok(new ContentResponse<>(Constants.EXECUTION_HISTORY, executionHistoryService.viewByTestGroupingId(id), RequestStatus.SUCCESS.getStatus(), statusCodeBundle.getCommonSuccessCode(), statusCodeBundle.getViewExecutionHistoryMessage()));
     }
 
     @GetMapping(EndpointURI.EXECUTION_HISTORY_ID)
     public ResponseEntity<String> viewReportByExecutionHistoryId(@PathVariable Long id) throws IOException {
         if (id == null) return ResponseEntity.ok(statusCodeBundle.getExecutionHistoryIdNull());
-        if (!executionHistoryService.existByExecutionHistoryId(id)) return ResponseEntity.ok(statusCodeBundle.getExecutionHistoryNotFound());
+        if (!executionHistoryService.existByExecutionHistoryId(id))
+            return ResponseEntity.ok(statusCodeBundle.getExecutionHistoryNotFound());
         return ResponseEntity.ok(executionHistoryService.viewReportByExecutionHistoryId(id));
     }
 }
