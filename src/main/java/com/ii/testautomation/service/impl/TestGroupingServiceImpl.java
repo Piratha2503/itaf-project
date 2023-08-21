bb62214cef5d8dd829c39df9c15de20d18edb28c
Vijinthika <vvijinthi@gmail.com>
2023-08-21 15:11:51 +0530

    TestGrouping save changing and Entity change

--- src/main/java/com/ii/testautomation/service/impl/TestGroupingServiceImpl.java
+++ src/main/java/com/ii/testautomation/service/impl/TestGroupingServiceImpl.java
@@ -52,7 +52,7 @@ public class TestGroupingServiceImpl implements TestGroupingService {
     @Autowired
     private ExecutedTestCaseRepository executedTestCaseRepository;
 
-    @Value("${jar.import.file.windows.path}")
+    @Value("${jar.import.file.ubuntu.path}")
     private String fileFolder;
 
     @Override
@@ -140,6 +140,8 @@ public class TestGroupingServiceImpl implements TestGroupingService {
             e.printStackTrace();
         }
         testGrouping.setExcelFilePath(filePaths);
+        Project project=projectRepository.findById(testGroupingRequest.getProjectId()).get();
+        testGrouping.setProject(project);
         testGroupingRepository.save(testGrouping);
     }
 
@@ -498,8 +500,13 @@ public class TestGroupingServiceImpl implements TestGroupingService {
     }
 
     @Override
-    public boolean existsByTestGroupingNameByProjectId(String name, Long projectId) {
+    public boolean existsByTestGroupingNameByTestCaseAndProjectId(String name, Long projectId) {
         return testGroupingRepository.existsByNameIgnoreCaseAndTestCases_SubModule_MainModule_Modules_Project_Id(name, projectId);
+    }
+
+    @Override
+    public boolean existsByTestGroupingNameByTestScenarioAndProjectId(String name, Long projectId) {
+        return testGroupingRepository.existsByNameIgnoreCaseAndTestScenarios_testCases_SubModule_MainModule_Modules_Project_Id(name,projectId);
     }
 
     @Override
