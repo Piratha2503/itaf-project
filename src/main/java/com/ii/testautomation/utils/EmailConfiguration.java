3c4eb18f42cd5e0b3767964a25199d5ccf7b6b40
Piratha2503 <pirathaban1992@gmail.com>
2023-08-28 18:19:09 +0530

    History Reports Email Service

--- src/main/java/com/ii/testautomation/utils/EmailConfiguration.java
+++ src/main/java/com/ii/testautomation/utils/EmailConfiguration.java
@@ -7,26 +7,25 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
-
 import java.util.Properties;
 
 @Configuration
 @Component
 @PropertySource("classpath:EmailSettings.properties")
 public class EmailConfiguration {
-    @Value("${spring.mail.password}")
-    private String emailPassword;
+
     @Value("${spring.mail.username}")
     private String emailUserName;
-
+    @Value("${spring.mail.password}")
+    private String emailPassword;
     @Bean
     public JavaMailSender javaMailSender()
     {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("pirathaban1992@gmail.com");
-        mailSender.setPassword("fwmadbagcxzslvoo");
+        mailSender.setUsername(emailUserName);
+        mailSender.setPassword(emailPassword);
 
         Properties properties = mailSender.getJavaMailProperties();
         properties.put("mail.smtp.starttls.enable", "true");
@@ -34,8 +33,5 @@ public class EmailConfiguration {
         properties.put("mail.transport.protocol", "smtp");
 
         return mailSender;
-
-
-
     }
 }
