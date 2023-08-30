b73395d136dc97e216bc5bfb9006f5030e73b425
TharsiSiva <tharsikasivakumaran@gmail.com>
2023-08-30 18:45:17 +0530

    ProgressBar

--- src/main/java/com/ii/testautomation/repositories/ProjectRepository.java
+++ src/main/java/com/ii/testautomation/repositories/ProjectRepository.java
@@ -12,4 +12,5 @@ public interface ProjectRepository extends JpaRepository<Project, Long>, Queryds
     boolean existsByNameIgnoreCaseAndIdNot(String name, Long id);
 
     boolean existsByCodeIgnoreCaseAndIdNot(String code, Long id);
+    
 }
