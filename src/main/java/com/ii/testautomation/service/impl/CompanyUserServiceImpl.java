91a1509b60d452111ce5d22f171dcce85d4abdcd
Vijinthika <vvijinthi@gmail.com>
2023-09-09 09:14:57 +0530

    changes

--- src/main/java/com/ii/testautomation/service/impl/CompanyUserServiceImpl.java
+++ src/main/java/com/ii/testautomation/service/impl/CompanyUserServiceImpl.java
@@ -1,26 +1,9 @@
 package com.ii.testautomation.service.impl;
 
-import com.ii.testautomation.dto.request.CompanyUserRequest;
-import com.ii.testautomation.dto.response.CompanyUserResponse;
-import com.ii.testautomation.dto.search.CompanyUserSearch;
-import com.ii.testautomation.entities.CompanyUser;
-import com.ii.testautomation.entities.QCompanyUser;
-import com.ii.testautomation.entities.Licenses;
-import com.ii.testautomation.enums.LoginStatus;
 import com.ii.testautomation.repositories.CompanyUserRepository;
-import com.ii.testautomation.repositories.LicenseRepository;
-import com.ii.testautomation.response.common.PaginatedContentResponse;
 import com.ii.testautomation.service.CompanyUserService;
-import com.ii.testautomation.utils.Utils;
-import com.querydsl.core.BooleanBuilder;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class CompanyUserServiceImpl implements CompanyUserService {
