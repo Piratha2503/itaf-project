1a8dc17148034f8c0a8a7d8009bc3d6ffd5ea3af
Piratha <piratha250392@yahoo.com>
2023-09-09 12:17:48 +0530

    company user modified

--- src/main/java/com/ii/testautomation/controllers/CompanyUserController.java
+++ src/main/java/com/ii/testautomation/controllers/CompanyUserController.java
@@ -1,10 +1,17 @@
+package com.ii.testautomation.controllers;
 
 import com.ii.testautomation.service.CompanyUserService;
 import com.ii.testautomation.utils.StatusCodeBundle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-@Autowired
-private StatusCodeBundle statusCodeBundle;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class CompanyUserController {
+    @Autowired
+    private StatusCodeBundle statusCodeBundle;
     @Autowired
     private CompanyUserService companyUserService;
+}
 
