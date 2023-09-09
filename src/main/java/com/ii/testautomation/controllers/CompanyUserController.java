7a3d54e77ef1122b34afb0bb7a0977eaad2ec4f9
Vijinthika <vvijinthi@gmail.com>
2023-09-09 09:15:07 +0530

    changes

--- src/main/java/com/ii/testautomation/controllers/CompanyUserController.java
+++ src/main/java/com/ii/testautomation/controllers/CompanyUserController.java
@@ -1,21 +1,10 @@
 package com.ii.testautomation.controllers;
 
-import com.ii.testautomation.dto.request.CompanyUserRequest;
-import com.ii.testautomation.dto.search.CompanyUserSearch;
-import com.ii.testautomation.entities.CompanyUser;
-import com.ii.testautomation.enums.RequestStatus;
-import com.ii.testautomation.response.common.BaseResponse;
-import com.ii.testautomation.response.common.PaginatedContentResponse;
 import com.ii.testautomation.service.CompanyUserService;
-import com.ii.testautomation.utils.Constants;
-import com.ii.testautomation.utils.EndpointURI;
 import com.ii.testautomation.utils.StatusCodeBundle;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
