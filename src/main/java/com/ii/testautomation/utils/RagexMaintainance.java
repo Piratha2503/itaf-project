f38f286639d7996d78d0757f0920c72058c0e79d
Piratha2503 <pirathaban1992@gmail.com>
2023-09-10 12:26:26 +0530

    changes modified

--- src/main/java/com/ii/testautomation/utils/RagexMaintainance.java
+++ src/main/java/com/ii/testautomation/utils/RagexMaintainance.java
@@ -7,9 +7,23 @@ import java.util.regex.Pattern;
 
 @Component
 public class RagexMaintainance {
-    public boolean checkSpaceBeforeAfterWords(String input)
+    public boolean checkSpaceBeforeAfterWordsTestCases(String input)
     {
         String regexPattern = "^(?!\\s)[A-Za-z0-9.]+(?:\\s[A-Za-z0-9.]+)*$";
+
+        Pattern pattern = Pattern.compile(regexPattern);
+
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean checkSpaceBeforeAfterWords(String input)
+    {
+        String regexPattern = "^(?!\\\\s)[A-Za-z0-9]+(?:\\\\s[A-Za-z0-9]+)*$";
 
         Pattern pattern = Pattern.compile(regexPattern);
 
