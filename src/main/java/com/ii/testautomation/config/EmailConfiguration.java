2093bf5fecf9ad365e4729b2f9d70d963b2bf30a
Piratha2503 <pirathaban1992@gmail.com>
2023-09-11 10:37:29 +0530

    Email changes ok

--- src/main/java/com/ii/testautomation/config/EmailConfiguration.java
+++ src/main/java/com/ii/testautomation/config/EmailConfiguration.java
@@ -1,4 +1,5 @@
-package com.ii.testautomation.utils;
+package com.ii.testautomation.config;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +11,8 @@ import java.util.Properties;
 
 @Configuration
 @Component
-@PropertySource("classpath:emailConfig.properties")
+@PropertySource("classpath:MessagesAndCodes.properties")
 public class EmailConfiguration {
-
     @Value("${spring.mail.username}")
     private String username;
     @Value("${spring.mail.password}")
@@ -49,5 +49,4 @@ public class EmailConfiguration {
         props.put("mail.debug", debug);
         return mailSender;
     }
-
 }
