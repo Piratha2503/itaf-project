7d5a7d26799bc952d807c39f1bb91acb60191dd8
Piratha <piratha250392@yahoo.com>
2023-09-12 03:17:52 +0530

    changes

--- src/main/java/com/ii/testautomation/service/UserService.java
+++ src/main/java/com/ii/testautomation/service/UserService.java
@@ -3,6 +3,7 @@ package com.ii.testautomation.service;
 import com.ii.testautomation.dto.request.UserRequest;
 
 public interface UserService {
+    void saveUser(UserRequest userRequest);
 
     void verifyUser(String token);
 
