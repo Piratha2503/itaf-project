ff5d3314fdf55be66c8f703ecc4d2d3d7b2d1234
KeerthananB <bkeerthiboss@gmail.com>
2023-09-14 17:28:42 +0530

    requiremoments

--- src/main/java/com/ii/testautomation/dto/search/UserSearch.java
+++ src/main/java/com/ii/testautomation/dto/search/UserSearch.java
@@ -1,11 +1,8 @@
 package com.ii.testautomation.dto.search;
-
 import lombok.Getter;
 import lombok.Setter;
-
 @Getter
 @Setter
-
 public class UserSearch {
     private String firstName;
     private String lastName;
