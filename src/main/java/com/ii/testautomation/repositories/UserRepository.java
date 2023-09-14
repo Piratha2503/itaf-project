21775c392dd09c5cc656263afe3e102b54bd0da5
KeerthananB <bkeerthiboss@gmail.com>
2023-09-14 17:27:17 +0530

    requiremoments

--- src/main/java/com/ii/testautomation/repositories/UserRepository.java
+++ src/main/java/com/ii/testautomation/repositories/UserRepository.java
@@ -11,29 +11,11 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<Users, Long>, QuerydslPredicateExecutor<Users> {
     boolean existsByEmailIgnoreCase(String email);
-
     List<Users> findByCompanyUserId(Long companyId);
-
     boolean existsByDesignationId(Long designationId);
-
     boolean existsByCompanyUserId(Long id);
-
     boolean existsByEmailIgnoreCaseAndIdNot(String email, Long id);
-
     boolean existsByContactNumberIgnoreCaseAndIdNot(String contactNumber, Long id);
-
     boolean existsByContactNumberIgnoreCase(String contactNo);
-
-//    Page<Users> findByCompanyUser_Id(Long companyUserId, Pageable pageable,BooleanBuilder booleanBuilder);
-
-    //Page<Users> findByCompanyUser_Id(BooleanBuilder booleanBuilder, Pageable pageable);
-
-    Page<Users> findAllByCompanyUserId(Long companyUserid, Pageable pageable,BooleanBuilder booleanBuilder);
-
     Users findByEmail(String email);
-
-    List<Users> findByCompanyUser_Id(Long companyUserId);
-
-
-
-}
+ }
