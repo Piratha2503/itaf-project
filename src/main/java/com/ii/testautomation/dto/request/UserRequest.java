ecc9805edaa77f69f7f7debc121f976c8226d6ef
TharsiSiva <tharsikasivakumaran@gmail.com>
2023-09-15 11:47:50 +0530

    changes

--- src/main/java/com/ii/testautomation/dto/request/UserRequest.java
+++ src/main/java/com/ii/testautomation/dto/request/UserRequest.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class UserRequest {
     private Long id;
     private String email;
+    private String password;
     private String firstName;
     private String lastName;
     private String contactNumber;
