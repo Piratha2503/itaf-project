eb476207b134020b25c44a460503068fb127d742
TharsiSiva <tharsikasivakumaran@gmail.com>
2023-09-22 15:07:14 +0530

    changes

--- src/main/java/com/ii/testautomation/controllers/UserController.java
+++ src/main/java/com/ii/testautomation/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.ii.testautomation.controllers;
 
 import com.ii.testautomation.dto.request.UserRequest;
+import com.ii.testautomation.dto.response.UserResponse;
 import com.ii.testautomation.dto.search.UserSearch;
 import com.ii.testautomation.enums.LoginStatus;
 import com.ii.testautomation.enums.RequestStatus;
@@ -30,6 +31,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
